5a06a11afe6a6a27d4cab654969bf678697d892c
Batman99SD <azozkrg1@gmail.com>
2024-05-06 09:27:16 -0700

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,44 +1,27 @@
 #!/usr/bin/env bash
-# script for setting up the web servers for deployment of web_static
+# a Bash script that sets up your web servers for the deployment of web_static
 
-#!/usr/bin/env bash
-# This script sets up web servers for the deployment of web_static.
+apt-get update
+apt-get -y install nginx
+# Install Nginx if it not already installed
+mkdir -p /data/web_static/releases/test/
+# Create the folder /data/ if it doesn’t already exist
 
-# Install nginx if not already installed
-if ! [ -x "$(command -v nginx)" ]; then
-    apt-get update
-    apt-get -y install nginx
-fi
-
-# Create necessary directories if they don't exist
-mkdir -p /data/web_static/releases/test /data/web_static/shared
-
-# Create a fake HTML file
-echo "<html>
-    <head>
-        <title>Test Page</title>
-    </head>
-    <body>
-        <p>This is a test page for web_static deployment.</p>
-    </body>
-</html>" | tee /data/web_static/releases/test/index.html
-
-# Create symbolic link, recreate if already exists
+mkdir -p /data/web_static/shared/
+# Create the folder /data/web_static/shared/ if it doesn’t already exist
+echo '<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>' > /data/web_static/releases/test/index.html
+# Create a fake HTML file /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Give ownership of /data/ folder to ubuntu user and group recursively
-chown -R ubuntu:ubuntu /data/
-
-# Update nginx configuration
-sed -i '/hbnb_static/ {
-    s/^\(\s*location\s*\)\(.*\)\(\{\)/\1\2\n\n\1\/hbnb_static\/ {\n\talias \/data\/web_static\/current\/;\n/g
-}' /etc/nginx/sites-available/default
-
-# Restart nginx
+# Create a symbolic link /data/web_static/current linked to the...
+# .../data/web_static/releases/test/ folder. If the symbolic link already..
+chown -hR ubuntu:ubuntu /data/
+# Give ownership of the /data/ folder to the ubuntu user AND group (you can
+sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+#restart Nginx after updating the configuration:
 service nginx restart
-
-if sudo nginx -t && sudo service nginx reload && curl -s localhost/hbnb_static | grep -q "This is a test page for web_static deployment."; then
-    echo "Nginx configuration updated and localhost/hbnb_static available"
-else
-    echo "Nginx configuration update failed or localhost/hbnb_static not available"
-fi
